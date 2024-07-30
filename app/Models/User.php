f275d64e8823f9e94f0d1d6b731a7f0f90c72698
Allan Rodrigues <allan.machado@shibata.com.br>
2024-07-30 13:47:57 -0300

    Hey-2: Refactoringo Code Error Test

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-//    use HasFactory;
+    use HasFactory;
     use Notifiable;
 
     /**
