86eac4c7933fae31c07b4a2be7bd93843480a7f8
Allan Rodrigues <allan.machado@shibata.com.br>
2024-07-30 18:35:44 -0300

    Hey-9: Refactoring method

--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -2,18 +2,20 @@
 
 use App\Models\User;
 
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, post};
+
 it('Should be able to create a new question bigger than 255 characters', function () {
     $user = User::factory()->create();
 
-    \Pest\Laravel\actingAs($user);
+    actingAs($user);
 
-    $request = \Pest\Laravel\post(route('question.store'), [
+    $request = post(route('question.store'), [
         'question' => str_repeat('*', 250) . '?',
     ]);
 
     $request->assertRedirect(route('dashboard'));
-    \Pest\Laravel\assertDatabaseCount('questions', 1);
-    \Pest\Laravel\assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
+    assertDatabaseCount('questions', 1);
+    assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
 });
 
 it('Should check if ends with question mark?', function () {
