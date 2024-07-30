e202bebc3fb428313911fbfed2d2076f09322374
Allan Rodrigues <allan.machado@shibata.com.br>
2024-07-30 11:07:30 -0300

    Hey-2: Setting larastan and fuxing debug

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory;
+//    use HasFactory;
     use Notifiable;
 
     /**
