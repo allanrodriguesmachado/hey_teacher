c2a608e425ed44cdff6c443b21035b6ab88844ca
unknown <allan.rodrigues14@hotmail.com>
2024-08-01 21:20:13 -0300

    Hey-9: Creating the frontend of our form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,10 +7,25 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+            <div>
+                <form class="mb-4" action="{{route('question.store')}}" method="post">
+                    @csrf
+                    <label for="question" class="block text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+                    <textarea  id="question" name="question" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here...">
+                        {{old('question')}}
+                    </textarea>
+
+                    @error('question')
+                        <span class="text-red-500">{{$message}}</span>
+                    @enderror
+
+                    <div class="flex justify-end mt-4">
+                        <button type="submit" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
+                        <button type="reset" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Cancel</button>
+                    </div>
+                </form>
+
+
             </div>
         </div>
     </div>
