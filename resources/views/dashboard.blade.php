8b46c98275a0268ecd9038827035672b72847282
unknown <allan.rodrigues14@hotmail.com>
2024-08-02 22:07:22 -0300

    Hey-9: Refactor to use blade components

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,24 +8,13 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div>
-                <form class="mb-4" action="{{route('question.store')}}" method="post">
-                    @csrf
-                    <label for="question" class="block text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-                    <textarea  id="question" name="question" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here...">
-                        {{old('question')}}
-                    </textarea>
+                <x-form method :action="route('question.store')" />
+                    <x-textarea label="Digite sua pergunta" name="question"/>
 
-                    @error('question')
-                        <span class="text-red-500">{{$message}}</span>
-                    @enderror
-
-                    <div class="flex justify-end mt-4">
-                        <button type="reset" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Cancel</button>
-                        <button type="submit" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
+                    <div class="flex justify-end">
+                        <x-btn.cancel name="Cancel" />
+                        <x-btn.submit name="Submit" />
                     </div>
-                </form>
-
-
             </div>
         </div>
     </div>
